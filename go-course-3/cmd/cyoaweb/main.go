a3d60b63a8d819026f8bfa3b9b65f262f12e4499
qwark97 <marcin.plata1@gmail.com>
2020-09-26 19:36:22 +0200

    minor refactoring

--- go-course-3/cmd/cyoaweb/main.go
+++ go-course-3/cmd/cyoaweb/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	cyoa "go-course/go-course-3"
@@ -15,16 +14,10 @@ func main() {
 	fmt.Printf("Using the story in %s.\n\n", *filename)
 
 	file, err := os.Open(*filename)
-	if err != nil {
-		panic(err)
-	}
+	cyoa.ErrHandle(err)
 
-	decoder := json.NewDecoder(file)
-	var story cyoa.Story
-
-	if err := decoder.Decode(&story); err != nil {
-		panic(err)
-	}
+	story, err := cyoa.JsonStory(file)
+	cyoa.ErrHandle(err)
 
 	fmt.Printf("%+v\n", story)
 }
--- go-course-3/story.go
+++ go-course-3/story.go
@@ -1,5 +1,27 @@
 package cyoa
 
+import (
+	"encoding/json"
+	"io"
+	"log"
+)
+
+func JsonStory(r io.Reader) (Story, error) {
+	var story Story
+	decoder := json.NewDecoder(r)
+
+	if err := decoder.Decode(&story); err != nil {
+		return nil, err
+	}
+	return story, nil
+}
+
+func ErrHandle(err error) {
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
 type Story map[string]Chapter
 
 type Chapter struct {
