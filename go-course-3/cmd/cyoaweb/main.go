e2e8a31dbd5221a183ebd22c4af3f37cbf211168
qwark97 <marcin.plata1@gmail.com>
2020-09-26 20:02:37 +0200

    implement running http server based on created handler to serve stories

--- go-course-3/cmd/cyoaweb/main.go
+++ go-course-3/cmd/cyoaweb/main.go
@@ -4,14 +4,17 @@ import (
 	"flag"
 	"fmt"
 	cyoa "go-course/go-course-3"
+	"log"
+	"net/http"
 	"os"
 )
 
 func main() {
+	port := flag.Int("port", 3000, "the port to start the CYOA web application on")
 	filename := flag.String("file", "gopher.json", "the JSON file file with CYOA story")
 	flag.Parse()
 
-	fmt.Printf("Using the story in %s.\n\n", *filename)
+	log.Printf("Using the story in %s.\n\n", *filename)
 
 	file, err := os.Open(*filename)
 	cyoa.ErrHandle(err)
@@ -19,5 +22,7 @@ func main() {
 	story, err := cyoa.JsonStory(file)
 	cyoa.ErrHandle(err)
 
-	fmt.Printf("%+v\n", story)
+	h := cyoa.NewHandler(story)
+	log.Printf("Starting the server on: http://localhost:%d\n", *port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), h))
 }
