fe70236b9ac06ca04f3d0b1ed9b5784afd1c70ad
qwark97 <marcin.plata1@gmail.com>
2020-09-26 20:02:08 +0200

    create http handler to generate stories

--- go-course-3/story.go
+++ go-course-3/story.go
@@ -4,7 +4,49 @@ import (
 	"encoding/json"
 	"io"
 	"log"
+	"net/http"
+	"text/template"
 )
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Choose Your Own Adwenture</title>
+    </head>
+    <body>
+        <h1>{{.Title}}</h1>
+        {{range .Paragraphs}}
+        <p>{{.}}</p>
+        {{end}}
+        <ul>
+            {{range .Options}}
+			<li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+            {{end}}
+        </ul>
+    </body>
+</html>
+`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := tpl.Execute(w, h.s["intro"])
+	ErrHandle(err)
+}
 
 func JsonStory(r io.Reader) (Story, error) {
 	var story Story
